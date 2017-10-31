d7df59a9357e9334b50b65f98edac1b85ec636d7
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-10-31 08:55:12 +0100

    script to sync with public GH

---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+COMMIT_MESSAGE=`date +"%Y-%m-%d %T%z"`' ('`git rev-parse HEAD`')'
+
+mkdir build
+git clone https://$PUBLIC_GITHUB_USER:$PUBLIC_GITHUB_TOKEN@github.com/l2fprod/a-repo-with-files build
+
+git config --global push.default simple
+git config --global user.email "autobuild@not-a-dom.ain"
+git config --global user.name "autobuild"
+
+# remove all files from checkout
+(cd build && git rm -rf .)
+
+# bring back all files from this commit
+tar cf - \
+  --exclude=build \
+  --exclude=.git \
+  --exclude=.gitignore \
+  --exclude=sync.sh \
+  --exclude=.travis.yml . | (cd build && tar xvf - )
+(cd build && git add . && git commit -m "$COMMIT_MESSAGE" && git push)
