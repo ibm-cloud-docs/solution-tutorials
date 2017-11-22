4011a89c268ae4b6279ef429bd1b56a393423c65
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-11-22 14:34:51 +0100

    can't require global packages

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -9,6 +9,8 @@ tar cf - \
   --exclude=README.md \
   . | (cd builddocs/input && tar xvf - )
 
-npm install -g marked-it-cli gh-pages
+npm install -g marked-it-cli
 marked-it-cli builddocs/input --output=builddocs/output --overwrite --header-file=scripts/header.txt
+
+(cd scripts && npm install)
 NODE_DEBUG=gh-pages node scripts/preview.js
