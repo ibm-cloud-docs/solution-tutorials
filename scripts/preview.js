d0a4dedb7e2cac2e9285a2cf75346cb56f043c62
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-11-22 14:46:32 +0100

    set user name, show error

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,5 +1,14 @@
 var ghpages = require('gh-pages');
 ghpages.publish('builddocs/output', {
   repo: 'https://' + process.env.GH_PAGES_TOKEN + '@github.ibm.com/Bluemix-Docs/tutorials.git',
-  silent: true
-}, function(err) {});
+  user: {
+    email: 'autobuild@not-a-dom.ain',
+    name: 'autobuild'
+  }
+}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Publish complete');
+  }
+});
