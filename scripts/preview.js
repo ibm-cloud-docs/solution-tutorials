61e66b1b027f4f5d737f60056d25be852113785e
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-11-22 14:26:40 +0100

    getting there...

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,5 +1,5 @@
 var ghpages = require('gh-pages');
 ghpages.publish('builddocs/output', {
-  repo: 'https://' + process.env.GH_PAGES_TOKEN + '@github.ibm.com/Bluemix-Docs/tutorials.git'
+  repo: 'https://' + process.env.GH_PAGES_TOKEN + '@github.ibm.com/Bluemix-Docs/tutorials.git',
   silent: true
 }, function(err) {});
