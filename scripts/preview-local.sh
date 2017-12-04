46b66a5d68f2b4aac2bdd2c83acd18963b3ed60b
Ram Vennam <rvennam987@gmail.com>
2017-12-04 15:04:46 -0600

    script to launch local test server

---
+++ scripts/preview-local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p builddocs/input
+
+# copy all files to doc input folder
+tar cf - \
+  --exclude=builddocs \
+  --exclude=scripts \
+  --exclude=.git \
+  --exclude=README.md \
+  . | (cd builddocs/input && tar xvf - )
+
+# generate the new files
+marked-it-cli builddocs/input --output=builddocs/output --overwrite --header-file=scripts/header.txt
+
+# start server
+http-server builddocs/output/
+
