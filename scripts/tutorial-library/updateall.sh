1ab276847fbb880ba2a98946db5166f98caa2049
Frederic Lavigne <frederic.lavigne@ibm.com>
2020-07-30 13:36:23 -0500

    removed file log

--- scripts/tutorial-library/updateall.sh
+++ scripts/tutorial-library/updateall.sh
@@ -3,6 +3,5 @@ set -e
 
 find ../.. -name '*.md' ! -path '*README*' ! -path '*index.md' ! -path '*/getting-started.md' ! -path '*/solution-template.md' -maxdepth 1 -print0 | while read -d $'\0' file
 do
-  echo "file: $file"
   node update.js $file false
 done
