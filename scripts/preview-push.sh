0495ea164a07c49dcaf61318c92d8cc2e6f84f90
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-07-12 13:40:29 -0500

    disable jekyll

--- scripts/preview-push.sh
+++ scripts/preview-push.sh
@@ -3,5 +3,8 @@ git config --global push.default simple
 git config --global user.email "autobuild@not-a-dom.ain"
 git config --global user.name "autobuild"
 
+# disable jekyll build as we push plain html
+(cd builddocs/output && touch .nojekyll)
+
 # commit to gh-pages
 (cd builddocs/output && git add . && [[ -z $(git status -uno --porcelain) ]] || (git commit -m "changes in draft" && git push))
