332017996d0d5d539ed32aa872b29c8fe6efbc2f
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-07-15 14:09:40 -0500

    new check for &#xa; character that breaks translation

---
+++ scripts/check-translation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+errorCode=0
+
+echo "Checking files for translation..."
+
+for file in $(find images -name '*.svg'); do
+  if grep -q '&#xa;' $file
+  then
+    echo "$file contains invalid &#xa; character. Edit the file with a text editor to remove occurrences"
+    errorCode=1
+  fi
+done
+
+if [ $errorCode == 0 ]; then
+  echo "No issue detected"
+fi
+
+echo exit $errorCode
